d1f05a89d23691e5f7ac14fdc168fa226ad1e40e
HoaiBaoNguyen <hoaibao2129@gmail.com>
2024-09-15 14:20:29 +0700

    reslove conflict

--- src/ts1.ts
+++ src/ts1.ts
@@ -1,15 +1,12 @@
 console.log("THIS IS TS1 FILE");
 
-<<<<<<< HEAD
 // commit 1
 console.log("Hello bao, how are you today?");
 
 // commit 2
 console.log("I'm good, and you?");
-=======
 //commit 1 from branch 2
 console.log("Hello Bao , Where are you from ?");
 
 //commit 2 from branch 2
 console.log("Oh, I am from VietNam, It a nice country");
->>>>>>> c6f5e23 (commit from branch 2)
